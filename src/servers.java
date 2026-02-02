a4ce93dcc1777663bb5e8dbe043c6531a54c4af3
Jonah St <jonahst10@gmail.com>
2026-02-01 20:26:33 -0800

    Updated servers

--- src/servers.java
+++ src/servers.java
@@ -1,22 +1,33 @@
 // Containers for server management
 
 abstract class ServerTemplate {
+
     // SUPERCLASS: Represents generated server instance's folder contents and management of .jar runtime
+    String serverName;
+
+    public void printName() {
+        System.out.println("Server name: " + serverName);
+    }
+    /*
+    public abstract void new(); // If existing server not found, generates new server
+    public abstract void start(); // Starts server runtime (server.jar, server.exe, ./bedrock_server, etc.)
+    public abstract void stop(); // Gracefully stops a running server runtime
+    */
 }
 
-final class JavaServer extends ServerTemplate {
+class BedrockServer extends ServerTemplate {
+    // SUBCLASS: Vanilla Bedrock Server
+    // TODO: Check host architecture before initializing; if is Linux AND is not Ubuntu.version >= 18, abort due to being unsupported
+}
+
+class JavaServer extends ServerTemplate {
     // SUBCLASS: Vanilla Java Minecraft
 }
 
-final class JavaModdedServer extends ServerTemplate {
+class FabricServer extends JavaServer {
     // SUBCLASS: Java Server with Fabric mod framework
 }
 
-final class BedrockServer extends ServerTemplate {
-    // SUBCLASS: Vanilla Bedrock Server
-    // TODO: Check host architecture before initializing; if is Linux AND is not Ubuntu.version >= 18, abort due to being unsupported by Microsoft
-}
-
-final class BridgeServer extends ServerTemplate {
-    // SUBCLASS: Java Server with GeyserMC/Floodgate bridge for Java/Bedrock crossover
+class BridgeServer extends FabricServer {
+    // SUBCLASS: Fabric Server with GeyserMC/Floodgate bridge for Java/Bedrock crossover
 }
