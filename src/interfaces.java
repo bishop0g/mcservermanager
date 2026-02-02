08559e63f2cac819ae5bd58c8cd78087a98876d4
Jonah St <jonahst10@gmail.com>
2026-02-01 20:26:10 -0800

    Removed "final" designations, Reorganized BridgeServer to inherit from FabricServer, which inherits from JavaServer.

--- src/interfaces.java
+++ src/interfaces.java
@@ -1,13 +1,14 @@
 // Determines if host is Linux or Windows, then provides interface functions for other modules.
+// import java.io.File;
 
 abstract class Interface {
     // Contains pre-formatted commands useful for server management
 }
 
-class UnixInterface extends Interface {
+final class UnixInterface extends Interface {
     // Linux- and macOS-formatted commands
 }
 
-class WindowsInterface extends Interface {
+final class WindowsInterface extends Interface {
     // Windows-formatted commands
 }
